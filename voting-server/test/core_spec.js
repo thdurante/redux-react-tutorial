11fc6f8e9b1cbc02aa163a408f9a5cc5c3466121
Thiago Durante Pires <thdurante@gmail.com>
2016-04-11 16:56:35 -0300

    finished loading entries

---
+++ voting-server/test/core_spec.js
@@ -0,0 +1,25 @@
+import {List, Map} from 'immutable';
+import {expect} from 'chai';
+
+import {setEntries} from '../src/core';
+
+describe('application logic', () => {
+    describe('setEntries', () => {
+        it('add the entries to state', () => {
+            const state = Map();
+            const entries = List.of('Trainspotting', '28 Days Later');
+            const nextState = setEntries(state, entries);
+            expect(nextState).to.equal(Map({
+                entries: List.of('Trainspotting', '28 Days Later')
+            }));
+        });
+        it('converts to immutable', () => {
+            const state = Map();
+            const entries = ['Trainspotting', '28 Days Later'];
+            const nextState = setEntries(state, entries);
+            expect(nextState).to.equal(Map({
+                entries: List.of('Trainspotting', '28 Days Later')
+            }));
+        });
+    });
+});
